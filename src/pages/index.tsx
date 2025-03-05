dfffe9cde6a06d0de4084de71beba5e1adb091b8
Balázs <bacs.balazs@kadeno.ro>
2025-03-06 00:11:27 +0200

    add tab titles and icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef, useState } from "react";
 import { User } from "@supabase/supabase-js";
 import supabaseClient from "@/lib/client";
 import Image from "next/image";
+import Head from "next/head";
 
 interface Profile {
   id: string;
@@ -151,151 +152,163 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className="flex min-h-screen bg-black text-white">
-      {/* Left Column (Chat messages and input) */}
-      <div className="w-4/5 border-r-2 border-orange-500 flex flex-col h-screen">
-        {/* Header */}
-        <div className="flex w-max mx-auto bg-white/30 backdrop-blur-3xl justify-center gap-3 mt-5 py-2 px-6 rounded-full items-center">
-          <h1 className="text-center text-4xl font-bold">Avocatel</h1>
-          <Image
-            src="/images/avocatel.webp"
-            alt="logo"
-            width={50}
-            height={50}
-          />
-        </div>
+    <>
+      <Head>
+        <title>Avocățel</title>
+        <meta
+          name="description"
+          content="Login or create an account to get info about laws."
+        />
+      </Head>
+      <div className="flex min-h-screen bg-black text-white">
+        {/* Left Column (Chat messages and input) */}
+        <div className="w-4/5 border-r-2 border-orange-500 flex flex-col h-screen">
+          {/* Header */}
+          <div className="flex w-max mx-auto bg-white/30 backdrop-blur-3xl justify-center gap-3 mt-5 py-2 px-6 rounded-full items-center">
+            <h1 className="text-center text-4xl font-bold">Avocatel</h1>
+            <Image
+              src="/images/avocatel.webp"
+              alt="logo"
+              width={50}
+              height={50}
+            />
+          </div>
 
-        {currentUser ? (
-          <>
-            {/* Messages Container */}
-            <div className="flex-1 overflow-y-auto p-8 space-y-6 scrollbar-none">
-              {messages.map((msg, index) => (
-                <div key={index} className="w-full flex flex-col gap-2 text-xl">
-                  <div className="py-3 px-6 bg-green-600 text-white rounded-3xl w-max max-w-xs sm:max-w-md">
-                    {msg}
-                  </div>
-                  <div className="w-full text-right flex justify-end">
-                    <div className="bg-gray-400 text-black py-3 px-6 rounded-3xl w-max max-w-xs sm:max-w-md">
-                      This is a demo response that will be replaced with api
-                      response.
+          {currentUser ? (
+            <>
+              {/* Messages Container */}
+              <div className="flex-1 overflow-y-auto p-8 space-y-6 scrollbar-none">
+                {messages.map((msg, index) => (
+                  <div
+                    key={index}
+                    className="w-full flex flex-col gap-2 text-xl"
+                  >
+                    <div className="py-3 px-6 bg-green-600 text-white rounded-3xl w-max max-w-xs sm:max-w-md">
+                      {msg}
+                    </div>
+                    <div className="w-full text-right flex justify-end">
+                      <div className="bg-gray-400 text-black py-3 px-6 rounded-3xl w-max max-w-xs sm:max-w-md">
+                        This is a demo response that will be replaced with api
+                        response.
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
-              <div ref={bottomRef} />
-            </div>
-
-            {/* Message input and send button */}
-            <div className="py-4 px-8 bg-black/90 flex gap-3 items-center">
-              <textarea
-                className="block w-full h-24 p-2 text-black rounded resize-none"
-                placeholder="Write something..."
-                value={draft}
-                onChange={(e) => setDraft(e.target.value)}
-                disabled={userCredits <= 0}
-              />
-              <button
-                type="button"
-                onClick={handleSend}
-                disabled={userCredits <= 0}
-                className={`h-max py-2 px-6 rounded-full text-lg font-semibold transition-colors ${
-                  userCredits > 0
-                    ? "bg-blue-600 hover:bg-blue-700 text-white"
-                    : "bg-gray-500 text-gray-300 cursor-not-allowed"
-                }`}
-              >
-                {userCredits > 0 ? "Send" : "Out of Credits"}
-              </button>
-            </div>
-          </>
-        ) : (
-          <div className="flex flex-1 justify-center items-center">
-            <Link
-              href="/login"
-              className="bg-blue-600 hover:bg-blue-700 text-white py-4 px-8 rounded-full text-2xl font-semibold transition-colors"
-            >
-              Sign in to start chatting
-            </Link>
-          </div>
-        )}
-      </div>
-
-      {/* Right Column (User info and actions) */}
-      <div className="w-1/5 p-4 bg-black/80 flex flex-col items-center">
-        {currentUser ? (
-          <div className="flex flex-col items-center">
-            <span className="text-green-500 mb-4 text-xl font-bold">
-              {currentUser.email}
-            </span>
-            <button
-              type="button"
-              onClick={handleLogout}
-              className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded-full text-lg font-semibold transition-colors"
-            >
-              Sign out
-            </button>
-
-            <div className="flex flex-col gap-3 w-full justify-center mt-10">
-              <p className="text-center text-lg font-semibold">
-                Available Credits
-              </p>
-              <div
-                className={`text-3xl text-center font-bold text-blue-500 transition-all transform ${
-                  userCredits !== prevCredits ? "scale-110 duration-300" : ""
-                }`}
-              >
-                {userCredits}
+                ))}
+                <div ref={bottomRef} />
               </div>
 
+              {/* Message input and send button */}
+              <div className="py-4 px-8 bg-black/90 flex gap-3 items-center">
+                <textarea
+                  className="block w-full h-24 p-2 text-black rounded resize-none"
+                  placeholder="Write something..."
+                  value={draft}
+                  onChange={(e) => setDraft(e.target.value)}
+                  disabled={userCredits <= 0}
+                />
+                <button
+                  type="button"
+                  onClick={handleSend}
+                  disabled={userCredits <= 0}
+                  className={`h-max py-2 px-6 rounded-full text-lg font-semibold transition-colors ${
+                    userCredits > 0
+                      ? "bg-blue-600 hover:bg-blue-700 text-white"
+                      : "bg-gray-500 text-gray-300 cursor-not-allowed"
+                  }`}
+                >
+                  {userCredits > 0 ? "Send" : "Out of Credits"}
+                </button>
+              </div>
+            </>
+          ) : (
+            <div className="flex flex-1 justify-center items-center">
+              <Link
+                href="/login"
+                className="bg-blue-600 hover:bg-blue-700 text-white py-4 px-8 rounded-full text-2xl font-semibold transition-colors"
+              >
+                Sign in to start chatting
+              </Link>
+            </div>
+          )}
+        </div>
+
+        {/* Right Column (User info and actions) */}
+        <div className="w-1/5 p-4 bg-black/80 flex flex-col items-center">
+          {currentUser ? (
+            <div className="flex flex-col items-center">
+              <span className="text-green-500 mb-4 text-xl font-bold">
+                {currentUser.email}
+              </span>
               <button
                 type="button"
-                onClick={() => setIsModalOpen(true)}
-                className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-6 rounded-full text-lg font-semibold transition-colors"
+                onClick={handleLogout}
+                className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded-full text-lg font-semibold transition-colors"
               >
-                Refill
+                Sign out
               </button>
+
+              <div className="flex flex-col gap-3 w-full justify-center mt-10">
+                <p className="text-center text-lg font-semibold">
+                  Available Credits
+                </p>
+                <div
+                  className={`text-3xl text-center font-bold text-blue-500 transition-all transform ${
+                    userCredits !== prevCredits ? "scale-110 duration-300" : ""
+                  }`}
+                >
+                  {userCredits}
+                </div>
+
+                <button
+                  type="button"
+                  onClick={() => setIsModalOpen(true)}
+                  className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-6 rounded-full text-lg font-semibold transition-colors"
+                >
+                  Refill
+                </button>
+              </div>
+            </div>
+          ) : (
+            <Link
+              href="/login"
+              className="bg-red-600 hover:bg-red-700 text-white py-3 px-6 rounded-full text-lg font-semibold transition-colors"
+            >
+              Sign In
+            </Link>
+          )}
+        </div>
+
+        {/* Refill Credits Modal */}
+        {isModalOpen && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+            <div className="bg-white text-black p-6 rounded-lg shadow-lg w-1/3">
+              <h2 className="text-2xl mb-4">Refill Credits</h2>
+              <p className="mb-4">Choose a refill package:</p>
+              <div className="flex flex-col gap-3">
+                <button
+                  className="bg-blue-600 text-white px-4 py-2 rounded"
+                  onClick={() => handleRefill(10)}
+                >
+                  10 Credits - 10 RON
+                </button>
+                <button
+                  className="bg-blue-800 text-white px-4 py-2 rounded"
+                  onClick={() => handleRefill(50)}
+                >
+                  50 Credits - 40 RON
+                </button>
+                <button
+                  className="bg-gray-500 text-white px-4 py-2 rounded"
+                  onClick={() => setIsModalOpen(false)}
+                >
+                  Cancel
+                </button>
+              </div>
             </div>
           </div>
-        ) : (
-          <Link
-            href="/login"
-            className="bg-red-600 hover:bg-red-700 text-white py-3 px-6 rounded-full text-lg font-semibold transition-colors"
-          >
-            Sign In
-          </Link>
         )}
       </div>
-
-      {/* Refill Credits Modal */}
-      {isModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white text-black p-6 rounded-lg shadow-lg w-1/3">
-            <h2 className="text-2xl mb-4">Refill Credits</h2>
-            <p className="mb-4">Choose a refill package:</p>
-            <div className="flex flex-col gap-3">
-              <button
-                className="bg-blue-600 text-white px-4 py-2 rounded"
-                onClick={() => handleRefill(10)}
-              >
-                10 Credits - 10 RON
-              </button>
-              <button
-                className="bg-blue-800 text-white px-4 py-2 rounded"
-                onClick={() => handleRefill(50)}
-              >
-                50 Credits - 40 RON
-              </button>
-              <button
-                className="bg-gray-500 text-white px-4 py-2 rounded"
-                onClick={() => setIsModalOpen(false)}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
+    </>
   );
 };
 
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useState } from "react";
 
 export default function Home() {
@@ -5,7 +6,7 @@ export default function Home() {
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-   
+
     const form = new FormData(e.target as HTMLFormElement);
     const inputs = Object.fromEntries(form.entries());
     await fetch("/api/login", {
@@ -18,38 +19,48 @@ export default function Home() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-black text-white">
-      <div className="w-full max-w-md bg-opacity-70 bg-black/80 rounded-lg p-8">
-        <h1 className="text-4xl font-bold text-center mb-8">
-          Ready to watch? Enter your email.
-        </h1>
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-300"
+    <>
+      <Head>
+        <title>Avocatel - Login</title>
+        <meta
+          name="description"
+          content="Login or create an account to get info about laws."
+        />
+      </Head>
+
+      <div className="flex justify-center items-center min-h-screen bg-black text-white">
+        <div className="w-full max-w-md bg-opacity-70 bg-black/80 rounded-lg p-8">
+          <h1 className="text-4xl font-bold text-center mb-8">
+            Ready to watch? Enter your email.
+          </h1>
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-300"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="mt-2 p-3 w-full border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-600 bg-black text-white placeholder-gray-400"
+                placeholder="Enter your email"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full py-3 px-6 bg-red-600 text-white rounded-lg text-lg font-semibold hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600"
             >
-              Email Address
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-              className="mt-2 p-3 w-full border border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-600 bg-black text-white placeholder-gray-400"
-              placeholder="Enter your email"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full py-3 px-6 bg-red-600 text-white rounded-lg text-lg font-semibold hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-600"
-          >
-            Get Started
-          </button>
-        </form>
+              Get Started
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
